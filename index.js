d5f8eb7053874c15085229ed358d496d0a176d6f
David Truong <dtruong1992@gmail.com>
2022-10-19 20:31:25 -0700

    Add get route for live data

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 require('dotenv').config()
+const API_KEY = process.env;
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 const path = require('path');
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
+const axios = require('axios');
 
-app.get('/', (req, res)=>{
-  res.json({message:'Welcome to Getflix!'})
+const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/';
+
+app.get('/main', async (req, res) => {
+  let movies = [];
+  for (var i = 1; i < 6; i++) {
+    let options = {
+      url: 'https://api.themoviedb.org/3/movie/popular',
+      params: {
+        api_key: API_KEY.KEY,
+        language: 'en-US',
+        page: i,
+        video: true
+      },
+      method: 'get',
+    }
+
+    await axios(options)
+      .then((data) => {
+        movies = movies.concat(data.data.results)
+      })
+      .catch ((error) => {
+        console.error(error)
+      })
+  }
+  console.log(movies)
+  res.send({data: movies})
 })
 
 app.listen(port, () => {
